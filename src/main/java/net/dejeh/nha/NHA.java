076149e00b91994b4df362cfd68cfc8eca935807
Dejeh <72144320+Dejeh@users.noreply.github.com>
2023-03-21 00:35:36 -0400

    Remove comment

--- src/main/java/net/dejeh/nha/NHA.java
+++ src/main/java/net/dejeh/nha/NHA.java
@@ -40,7 +40,6 @@ public class NHA {
         }
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
